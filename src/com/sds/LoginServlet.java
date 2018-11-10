189eebc521d1db7c7e2c1184ceeabafcf677a566
swapnildshelke <swapnildshelke@gmail.com>
2018-11-10 14:34:49 +0530

    jsp to servlet data transfer

--- src/com/sds/LoginServlet.java
+++ src/com/sds/LoginServlet.java
@@ -2,6 +2,7 @@ package com.sds;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +11,26 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
 	@Override
 	public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//Class.forName("LoginServlet").newInstance();
 		System.out.println("I am in service");
+		//GET DATA
+		String un = req.getParameter("uname");
+		System.out.println("Username >>"+un);
+		
+		//Processing of data
+		boolean isCorrect=false;
+		if("sds".equals(un)) {
+			isCorrect=true;
+		}
+		
+		//navigation to other pages
+		RequestDispatcher rd =null;
+		if(isCorrect) {
+			 rd = req.getRequestDispatcher("inbox.html");
+		}else {
+			 rd = req.getRequestDispatcher("login.html");
+		}
+		
+		rd.forward(req, resp);
 	}
 }
